aec5e86b91017bbffe86778de701b3092c64af8a
Solid Works - Development & Marketing <cesurenes35++@gmail.com>
2024-08-15 09:47:59 +0300

    Update solana-volume-bot.py

--- solana-volume-bot.py
+++ solana-volume-bot.py
@@ -7,3 +7,9 @@ print("Please read README.MD file for details!")
 print("Please read README.MD file for details!")
 
 print("Please read README.MD file for details!")
+
+"""
+Copyright © 2024 t.me/solid_marketing
+This code is the property of Solid Marketing. Unauthorized copying,
+modification, distribution, or other use of this code is prohibited.
+"""
